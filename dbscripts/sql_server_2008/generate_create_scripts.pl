7fce801e90ed827624ca8bc78912e037d4766726
smoore <>
2009-09-07 03:34:00 +0000

    Do a better job of searching for SQL server base folder and data files and giving the user the opportunity to opt out and pick different folders.

--- dbscripts/sql_server_2008/generate_create_scripts.pl
+++ dbscripts/sql_server_2008/generate_create_scripts.pl
@@ -3,11 +3,14 @@
 sub prompt_for_sql_server
 {
   my $done = 0;
-  my $folder = "";
-  print "Default folder for SQL Server is C:\Program Files\Microsoft SQL Server";
+  my $folder = "C:\\Program Files\\Microsoft SQL Server";
+  print "Default folder for SQL Server is $folder \n";
+  if (-e $folder) { print "Default folder does exist \n"; }
+  else { print "Default folder DOES NOT exist \n";}   
+  
 
   while ($done == 0) {
-    print "Enter folder for SQL Server (enter for default) ";
+    print "Enter folder for SQL Server (just hit ENTER for default) ";
     $folder = <STDIN>;
     if ($folder eq "" || $folder eq "\r" || $folder eq "\n") {
       $folder = "C:\\Program Files\\Microsoft SQL Server";
@@ -40,17 +43,45 @@ sub addDataFolder
 {
   my ($f) = @_;
 
-  $x = $f . "\\MSSQL\\DATA";
-  if (-e $x) {
-    return $x;
+  # First, look to see if any of the SQL server data folders exist
+  my @allFolders = ( $f . "\\MSSQL10.SQLEXPRESS2008\\MSSQL\\DATA",
+  	$f . "\\MSSQL.1\\MSQL\\DATA",
+  	$f . "\\MSQL\\DATA" );
+
+  print "\nNow looking for data folders in $f\n";
+  my $idx = 0;
+  my @existingFolders;
+  foreach $folder(@allFolders) {
+    if (-e $folder) {
+      $existingFolders[$idx] = $folder;
+      $idx++;
+      print "$idx $folder \n"; }
   }
 
-  $x1 = $f . "\\MSSQL.1\\MSSQL\\DATA";
-  if (-e $x1) {
-    return $x1;
+  # Show the user folders we found and ask if any of these are to be used
+  if ($idx != 0) {
+    my $idxChoice = -1;
+    while ($idxChoice > $idx || $idxChoice < 0) {
+      print "\nWe have found one or more folders for SQL Server Data\n";
+      print "Select one of the folders (1, 2, ...)\n   or enter 0 to enter a different name -> ";
+      $idxChoice = <STDIN>;
+      if ($idxChoice eq "" || $idxChoice eq "\r" || $idxChoice eq "\n") { $idxChoice = -1; }
+    }
+    if ($idxChoice != 0) {
+      print "Selected: $existingFolders[$idxChoice-1]\n";
+      return $existingFolders[$idxChoice-1];
+    }
   }
 
-  die "Could find $x nor $x1; this is a bug in our expectation of the SQL Server installation";
+
+  # Either no folders were found, or the user wants to select a different one
+  my $newFolder = "XX";
+  while (! -e $newFolder) {
+    print "Enter path to data folder for SQL Server Data \n --> ";
+    $newFolder = <STDIN>;
+    chomp $newFolder;
+  }
+  return $newFolder;
 }
 
 
